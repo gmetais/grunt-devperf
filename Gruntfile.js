6273e45781f52793272e35e04378c1a830dc5bf8
Gaël Métais <gael.metais@gmail.com>
2014-04-25 15:34:32 +0200

    Simplify Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,51 +6,31 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
 module.exports = function(grunt) {
+  'use strict';
 
   // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
         'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
+        'tasks/*.js'
+      ]
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
+    // Configuration to be run
     devperf: {
       options: {
         urls: [
-          'http://www.francetvinfo.fr',
-          'http://www.francetvsport.fr',
-          'http://culturebox.francetvinfo.fr',
-          'http://www.france3.fr/emissions/plus-belle-la-vie',
-          'http://programme-tv.francetv.fr/',
-          'http://alsace.france3.fr/',
-          'http://pluzz.francetv.fr',
-          'http://www.france2.fr',
-          'http://www.france3.fr',
-          'http://www.france4.fr',
-          'http://www.france5.fr',
-          'http://www.franceo.fr'
+          'http://www.google.com',
+          'http://www.yahoo.com'
         ]
       }
     },
 
-    // Unit tests.
+    // Unit tests : TODO
     nodeunit: {
-      tests: ['test/*_test.js']
+      tests: []
     }
 
   });
@@ -61,15 +41,11 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'devperf', 'nodeunit']);
 
-  grunt.registerTask('perf', ['devperf']);
+  grunt.registerTask('default', ['devperf']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('lint', ['jshint']);
 
 };
